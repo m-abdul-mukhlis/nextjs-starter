fba2c8045068df8fdcf7c0675974e7b16f31a530
mukhlis <mukhlis@fisip.net>
2024-03-06 16:34:52 +0700

    mark css

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import styles from '@/app/ui/home.module.css';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -15,7 +14,7 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           {/* css */}
-          <div className={styles.shape} />
+          {/* <div className={styles.shape} /> */}
 
           {/* tailwind */}
           <div
